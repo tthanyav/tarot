512b2b9e300f063f1d82d59e45366714121848c9
Phillionare <zalaterx@gmail.com>
2026-02-02 13:15:21 +0800

    Move reversed cards toggle to bottom of spread selection (v1.1.1)

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // Version file - update this when deploying major changes
-const APP_VERSION = "1.1.0";
-const LAST_UPDATED = "2026-01-31 00:10";
+const APP_VERSION = "1.1.1";
+const LAST_UPDATED = "2026-01-31 00:15";
